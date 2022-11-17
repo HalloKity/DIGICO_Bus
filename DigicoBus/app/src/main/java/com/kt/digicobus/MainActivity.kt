8ba6ade431dca7a86ff3b67ed40a503513869ae4
Song HyeonJin <40493508+ssonghj@users.noreply.github.com>
2022-11-17 17:19:34 +0900

    [HJ] feat : SplashScreen 추가

--- DigicoBus/app/src/main/java/com/kt/digicobus/MainActivity.kt
+++ DigicoBus/app/src/main/java/com/kt/digicobus/MainActivity.kt
@@ -2,9 +2,11 @@ package com.kt.digicobus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
---
+++ DigicoBus/app/src/main/java/com/kt/digicobus/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.kt.digicobus
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 1000)
+
+    }
+}
