bfc987ad93f85f45aaba2256ebdb0aee7db71ade
MunYeong0723 <ansdud0723@naver.com>
2022-11-30 15:32:40 +0900

    [MY] feat : 로그인 기능 추가

--- DigicoBus/app/src/main/java/com/kt/digicobus/login/LoginActivity.kt
+++ DigicoBus/app/src/main/java/com/kt/digicobus/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.kt.digicobus.login
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kt.digicobus.MainActivity
 import com.kt.digicobus.R
@@ -25,9 +26,24 @@ class LoginActivity : AppCompatActivity() {
         }
 
         binding.btnLogin.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-            startActivity(intent)
+            // 로그인
+            val userId = binding.editId.text.toString()
+            val userPw = binding.editPassword.text.toString()
+
+            val loginService = LoginService()
+            if (loginService.requestLogin(userId, userPw)) {
+                // 로그인 성공
+                val intent = Intent(this, MainActivity::class.java)
+
+                intent.putExtra("userId", userId)
+                intent.putExtra("userPw", userPw)
+
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(intent)
+            } else {
+                // 로그인 실패
+                Toast.makeText(applicationContext, "로그인에 실패하셨습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
 
 
