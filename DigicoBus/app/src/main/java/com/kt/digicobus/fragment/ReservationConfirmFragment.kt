06806bddeb98a9e24dec57eba92160d0bc97ed10
Song HyeonJin <40493508+ssonghj@users.noreply.github.com>
2022-12-02 16:43:55 +0900

    [HJ] feat : 예약확인 어댑터 결합

--- DigicoBus/app/src/main/java/com/kt/digicobus/fragment/ReservationConfirmFragment.kt
+++ DigicoBus/app/src/main/java/com/kt/digicobus/fragment/ReservationConfirmFragment.kt
@@ -1,17 +1,33 @@
 package com.kt.digicobus.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.kt.digicobus.R
+import com.kt.digicobus.adapter.ReservationConfirmAdapter
+import com.kt.digicobus.adapter.TicketListAdapter
+import com.kt.digicobus.data.data
 import com.kt.digicobus.databinding.FragmentCommuteBinding
 import com.kt.digicobus.databinding.FragmentMoreBinding
 import com.kt.digicobus.databinding.FragmentReservationConfirmBinding
+import me.everything.android.ui.overscroll.OverScrollDecoratorHelper
 
 class ReservationConfirmFragment : Fragment() {
     private lateinit var binding : FragmentReservationConfirmBinding
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var reservationConfirmAdapter: ReservationConfirmAdapter
+
+    private lateinit var ctx: Context
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        ctx = context
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +35,22 @@ class ReservationConfirmFragment : Fragment() {
     ): View? {
         binding = FragmentReservationConfirmBinding.inflate(layoutInflater)
 
+        setAdapter()
+
         return binding.root
+    }
+
+    private fun setAdapter(){
+
+        // RecyclerView 객체 생성
+        recyclerView = binding.recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(ctx, LinearLayoutManager.VERTICAL, false)
+        OverScrollDecoratorHelper.setUpOverScroll(recyclerView, OverScrollDecoratorHelper.ORIENTATION_VERTICAL)
+
+        // 2. Adapter 객체 생성(한 행을 위해 반복 생성할 Layout과 데이터 전달)
+        reservationConfirmAdapter = ReservationConfirmAdapter(ctx, R.layout.card_reservation_confirm, data.ticketList)
+
+        // 3. RecyclerView와 Adapter 연결
+        recyclerView.adapter = reservationConfirmAdapter
     }
 }
