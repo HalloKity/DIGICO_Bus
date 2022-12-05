147b26b387b5d1196741c1b7f4647ee5d27b3ede
MunYeong0723 <ansdud0723@naver.com>
2022-12-05 15:07:17 +0900

    [MY] refactor : 네이버지도에 표시하는 부분의 코드 정리

--- DigicoBus/app/src/main/java/com/kt/digicobus/fragment/shuttle/ShuttleFragment.kt
+++ DigicoBus/app/src/main/java/com/kt/digicobus/fragment/shuttle/ShuttleFragment.kt
@@ -1,31 +1,21 @@
 package com.kt.digicobus.fragment.shuttle
 
 import android.content.Context
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import com.kt.digicobus.NaverMapAPI
 import com.kt.digicobus.R
-import com.kt.digicobus.data.ResultPath
 import com.kt.digicobus.databinding.FragmentShuttleBinding
+import com.kt.digicobus.naverMap.NaverMapAPIService
 import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.*
-import com.naver.maps.map.overlay.Marker
-import com.naver.maps.map.overlay.PathOverlay
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import com.kt.digicobus.BuildConfig
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
 class ShuttleFragment : Fragment(), OnMapReadyCallback {
     private lateinit var binding: FragmentShuttleBinding
@@ -64,78 +54,22 @@ class ShuttleFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(naverMap: NaverMap) {
-        // 판교 사옥 위치
+        val naverMapAPIService = NaverMapAPIService(naverMap)
+
+        // 판교사옥, 판교역 위치
         val latlngOffice = LatLng(37.407385, 127.090450)
         val latlngStation = LatLng(37.394769, 127.111194)
 
         // camera position
-        val cameraUpdate = CameraUpdate.scrollTo(latlngOffice)
-        naverMap.moveCamera(cameraUpdate)
+        naverMapAPIService.setCameraPosition(latlngOffice)
 
         // 판교사옥 마커 표시
-        val markerOffice = Marker()
-        markerOffice.position = latlngOffice
-        markerOffice.width = Marker.SIZE_AUTO
-        markerOffice.height = Marker.SIZE_AUTO
-        markerOffice.captionText = "KT 판교사옥"
-        markerOffice.map = naverMap
+        naverMapAPIService.setMarker(latlngOffice, "KT 판교사옥")
 
         // 판교역 마커 표시
-        val markerStation = Marker()
-        markerStation.position = latlngStation
-        markerStation.width = Marker.SIZE_AUTO
-        markerStation.height = Marker.SIZE_AUTO
-        markerStation.map = naverMap
+        naverMapAPIService.setMarker(latlngStation)
 
-        // ============ 경로 표시 ===============
-        val APIKEY_ID = BuildConfig.NAVER_MAP_CLIENT_ID
-        val APIKEY = BuildConfig.NAVER_MAP_CLIENT_SECRET
-
-        val retrofit =
-            Retrofit.Builder().baseUrl("https://naveropenapi.apigw.ntruss.com/map-direction/")
-                .addConverterFactory(GsonConverterFactory.create()).build()
-        val api = retrofit.create(NaverMapAPI::class.java)
-
-        val callgetPath = api.getPath(
-            APIKEY_ID,
-            APIKEY,
-            "${latlngStation.longitude}, ${latlngStation.latitude}",
-            "${latlngOffice.longitude}, ${latlngOffice.latitude}",
-        )
-
-        callgetPath.enqueue(object : Callback<ResultPath> {
-            override fun onResponse(
-                call: Call<ResultPath>,
-                response: Response<ResultPath>
-            ) {
-                val path_cords_list = response.body()?.route?.traoptimal
-
-                //경로 그리기 응답바디가 List<List<Double>> 이라서 2중 for문 썼음
-                val path = PathOverlay()
-                //MutableList에 add 기능 쓰기 위해 더미 원소 하나 넣어둠
-                val path_container: MutableList<LatLng>? = mutableListOf(LatLng(0.1, 0.1))
-                for (path_cords in path_cords_list!!) {
-                    for (path_cords_xy in path_cords?.path!!) {
-                        //구한 경로를 하나씩 path_container에 추가해줌
-                        path_container?.add(LatLng(path_cords_xy[1], path_cords_xy[0]))
-                    }
-                }
-                //더미원소 드랍후 path.coords에 path들을 넣어줌.
-                path.coords = path_container?.drop(1)!!
-                path.color = Color.RED
-                path.map = naverMap
-
-                // 경로를 보여주도록 카메라 이동
-                if (path.coords != null) {
-                    val cameraUpdate = CameraUpdate.scrollAndZoomTo(path.coords[path.coords.size/2-8]!!, 12.5)
-                        .animate(CameraAnimation.Fly, 3000)
-                    naverMap!!.moveCamera(cameraUpdate)
-                }
-            }
-
-            override fun onFailure(call: Call<ResultPath>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
-        })
+        // 판교역-판교사옥 경로 표시
+        naverMapAPIService.setPath(latlngStation, latlngOffice)
     }
 }
---
+++ DigicoBus/app/src/main/java/com/kt/digicobus/naverMap/NaverMapAPIService.kt
@@ -0,0 +1,92 @@
+package com.kt.digicobus.naverMap
+
+import android.graphics.Color
+import com.kt.digicobus.BuildConfig
+import com.kt.digicobus.data.ResultPath
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraAnimation
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.PathOverlay
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NaverMapAPIService(naverMapParam: NaverMap) {
+    private val APIKEY_ID = BuildConfig.NAVER_MAP_CLIENT_ID
+    private val APIKEY = BuildConfig.NAVER_MAP_CLIENT_SECRET
+    private val naverMap: NaverMap
+
+    init {
+        naverMap = naverMapParam
+    }
+
+    fun setCameraPosition(latlng: LatLng) {
+        val cameraUpdate = CameraUpdate.scrollTo(latlng)
+        naverMap.moveCamera(cameraUpdate)
+    }
+
+    fun setMarker(latlng: LatLng, text: String = "") {
+        val marker = Marker()
+
+        marker.position = latlng
+        marker.width = Marker.SIZE_AUTO
+        marker.height = Marker.SIZE_AUTO
+        marker.captionText = text
+        marker.map = naverMap
+    }
+
+    fun setPath(start: LatLng, goal: LatLng) {
+        val retrofit =
+            Retrofit.Builder().baseUrl("https://naveropenapi.apigw.ntruss.com/map-direction/")
+                .addConverterFactory(GsonConverterFactory.create()).build()
+
+        val api = retrofit.create(NaverMapAPI::class.java)
+        val callgetPath = api.getPath(
+            APIKEY_ID,
+            APIKEY,
+            "${start.longitude}, ${start.latitude}",
+            "${goal.longitude}, ${goal.latitude}",
+        )
+
+        // api 호출
+        callgetPath.enqueue(object : Callback<ResultPath> {
+            override fun onResponse(
+                call: Call<ResultPath>,
+                response: Response<ResultPath>
+            ) {
+                val path_cords_list = response.body()?.route?.traoptimal
+
+                //경로 그리기 응답바디가 List<List<Double>> 이라서 2중 for문 썼음
+                val path = PathOverlay()
+                //MutableList에 add 기능 쓰기 위해 더미 원소 하나 넣어둠
+                val path_container: MutableList<LatLng>? = mutableListOf(LatLng(0.1, 0.1))
+                for (path_cords in path_cords_list!!) {
+                    for (path_cords_xy in path_cords?.path!!) {
+                        //구한 경로를 하나씩 path_container에 추가해줌
+                        path_container?.add(LatLng(path_cords_xy[1], path_cords_xy[0]))
+                    }
+                }
+                //더미원소 드랍후 path.coords에 path들을 넣어줌.
+                path.coords = path_container?.drop(1)!!
+                path.color = Color.RED
+                path.map = naverMap
+
+                // 경로를 보여주도록 카메라 이동
+                if (path.coords != null) {
+                    val cameraUpdate =
+                        CameraUpdate.scrollAndZoomTo(path.coords[path.coords.size / 2]!!, 12.5)
+                            .animate(CameraAnimation.Fly, 3000)
+                    naverMap!!.moveCamera(cameraUpdate)
+                }
+            }
+
+            override fun onFailure(call: Call<ResultPath>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+}
