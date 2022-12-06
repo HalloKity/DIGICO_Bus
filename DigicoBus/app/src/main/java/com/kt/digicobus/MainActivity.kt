ea3f3aeb453d6b4871d32729dd4275cafb0151a6
Song HyeonJin <40493508+ssonghj@users.noreply.github.com>
2022-12-06 21:30:23 +0900

    [HJ] feat : 예약확인 페이지 전화걸기 추가

--- DigicoBus/app/src/main/java/com/kt/digicobus/MainActivity.kt
+++ DigicoBus/app/src/main/java/com/kt/digicobus/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
 
         val navController = navHostFragment.navController
--- DigicoBus/app/src/main/java/com/kt/digicobus/adapter/ReservationConfirmAdapter.kt
+++ DigicoBus/app/src/main/java/com/kt/digicobus/adapter/ReservationConfirmAdapter.kt
@@ -1,28 +1,35 @@
 package com.kt.digicobus.adapter
 
-import android.annotation.SuppressLint
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
+import com.kt.digicobus.MainActivity
 import com.kt.digicobus.R
 import com.kt.digicobus.data.TicketContent
 import com.kt.digicobus.dialog.MyBottomDialogQrcodeHelp
-import com.kt.digicobus.dialog.MyBottomDialogStopLocation
-import com.kt.digicobus.fragment.commute.CommuteBusChoiceFragment
-import com.kt.digicobus.fragment.commute.CommuteFragment
-import com.kt.digicobus.fragment.commute.CommuteMainFragment
 import kotlinx.android.synthetic.main.listview_ticket.view.*
+import org.w3c.dom.Text
 
 class ReservationConfirmAdapter(var context: Context, private val resource: Int,  var ticketContentsList: MutableList<TicketContent>)
     : RecyclerView.Adapter<ReservationHolder>() {
+    val PERMISSIONS_CALL_PHONE = 1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReservationHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -40,10 +47,47 @@ class ReservationConfirmAdapter(var context: Context, private val resource: Int,
                 bottomDialog.tag
             )
         }
+
+        val callNumber = holder.tv_car_number_content.text.toString()
+        holder.tv_car_number_content.setOnClickListener{
+            dialog(callNumber)
+        }
+
+        holder.icon_call.setOnClickListener{
+            dialog(callNumber)
+        }
     }
 
     override fun getItemCount(): Int {
         return ticketContentsList.size
+    }
+
+    fun dialog(callNumber: String){
+        //권한확인 후 다이얼로그 뷰 띄우기
+        AlertDialog.Builder(context)
+            .setMessage("${callNumber}로 전화하시겠습니까?")
+            .setPositiveButton("확인", object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface, which: Int) {
+                    if (ContextCompat.checkSelfPermission(
+                            context.applicationContext,
+                            Manifest.permission.CALL_PHONE
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        ActivityCompat.requestPermissions(
+                            context as Activity,
+                            arrayOf(Manifest.permission.CALL_PHONE),
+                            PERMISSIONS_CALL_PHONE
+                        )
+                    } else {
+                        val callIntent = Intent(Intent.ACTION_CALL)
+                        callIntent.data = Uri.parse("tel:${callNumber}")
+                        context.startActivity(callIntent)
+                    }
+                }
+            })
+            .setNegativeButton("취소") { dialog, which -> }
+            .create()
+            .show()
     }
 }
 
@@ -55,5 +99,8 @@ class ReservationHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val tv_to_time: TextView = itemView!!.findViewById(R.id.tv_to_time)
     val tv_from_time: TextView = itemView!!.findViewById(R.id.tv_from_time)
     val google_map: ImageView = itemView!!.findViewById(R.id.google_map)
+
+    var tv_car_number_content: TextView = itemView!!.findViewById((R.id.tv_car_number_content))
+    var icon_call: ImageView = itemView!!.findViewById(R.id.icon_call)
     val icon_info: ImageView = itemView!!.findViewById(R.id.icon_info)
 }
