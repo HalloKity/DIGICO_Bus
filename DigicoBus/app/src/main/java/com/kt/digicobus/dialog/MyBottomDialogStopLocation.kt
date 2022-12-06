98da1523588428feccdf2a4707794375c118abaf
MunYeong0723 <ansdud0723@naver.com>
2022-12-06 16:22:51 +0900

    [MY] feat : 정차 위치 bottom dialog에 마커 표시

--- DigicoBus/app/src/main/java/com/kt/digicobus/dialog/MyBottomDialogStopLocation.kt
+++ DigicoBus/app/src/main/java/com/kt/digicobus/dialog/MyBottomDialogStopLocation.kt
@@ -1,14 +1,24 @@
 package com.kt.digicobus.dialog
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.kt.digicobus.R
+import com.kt.digicobus.naverMap.NaverMapAPIService
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MyBottomDialogStopLocation : BottomSheetDialogFragment() {
+class MyBottomDialogStopLocation : BottomSheetDialogFragment(), OnMapReadyCallback {
+    private lateinit var mapView: MapView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,5 +33,39 @@ class MyBottomDialogStopLocation : BottomSheetDialogFragment() {
         view.findViewById<Button>(R.id.btn_close)?.setOnClickListener {
             dismiss()
         }
+
+        // dialog가 drag로 닫히지 않도록 설정
+        if (dialog is BottomSheetDialog) {
+            val behaviour = (dialog as BottomSheetDialog).behavior
+            behaviour.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    if (newState == BottomSheetBehavior.STATE_DRAGGING) {
+                        behaviour.state = BottomSheetBehavior.STATE_EXPANDED
+                    }
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                }
+            })
+        }
+
+        // 네이버 지도
+        mapView = view.findViewById(R.id.station_map)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        val naverMapAPIService = NaverMapAPIService(naverMap)
+
+        // 판교사옥, 판교역 위치
+        val latlngOffice = LatLng(37.407385, 127.090450)
+
+        // camera position
+        naverMapAPIService.setCameraPosition(latlngOffice)
+
+        // 판교사옥 마커 표시
+        naverMapAPIService.setMarker(latlngOffice, "KT 판교사옥")
     }
 }
