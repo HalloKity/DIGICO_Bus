8a13dafb1039d5c16c4e43530cd3371e1f30cad8
Song HyeonJin <40493508+ssonghj@users.noreply.github.com>
2022-12-15 13:30:33 +0900

    [HJ] fix : 모바일케이트 화면 추가

--- DigicoBus/app/src/main/java/com/kt/digicobus/activity/MainActivity.kt
+++ DigicoBus/app/src/main/java/com/kt/digicobus/activity/MainActivity.kt
@@ -16,9 +16,6 @@ import com.kt.digicobus.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    // 뒤로가기 버튼을 눌렀던 시간 저장
-    private var backKeyPressedTime: Long = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -49,22 +46,4 @@ class MainActivity : AppCompatActivity() {
         }
         return super.dispatchTouchEvent(ev)
     }
-
-    override fun onBackPressed() {
-        // 기존의 뒤로가기 버튼의 기능 제거
-        // super.onBackPressed();
-
-        // 2000 milliseconds = 2 seconds
-        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
-            backKeyPressedTime = System.currentTimeMillis()
-            Toast.makeText(this, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        // 2초 이내에 뒤로가기 버튼을 한번 더 클릭시 finish()(앱 종료)
-        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
-            finish()
-        }
-    }
-
 }
